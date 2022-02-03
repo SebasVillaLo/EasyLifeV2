b424f79cb65e8c73a6706c0362ac134ff6046c44
SebasVillaLo <villalopezsebastian@gmail.com>
2022-02-02 23:44:38 -0500

    Realest 2.0

--- EasyLife.py
+++ EasyLife.py
@@ -3,40 +3,38 @@ import subprocess
 from collections import OrderedDict
 import os
 from os.path import exists
-import urllib.request as request
-from sys import argv
+from colorama import init, Fore
 
 try:
+    init()
     todosArchivos = []
-    print("Bienvenido, espero sea se tu agrado mi primer script. :D\n")
-    print("-----------------------------------------------------------------\n")
-    print(
-        "Asegurate de estar en la carpeta donde vas a crear la carpeta y los archivos")
-    print("Ya sea el low leves o el higher level, o en la carpeta que gustes.")
-    print("-----------------------------------------------------------------\n")
-    path = input(
-        "Ingrese a continuacion el nombre de la carpeta donde van todos los archivos (ejem: 0x15-python_hola): ").lower().strip("/")
+    print(Fore.BLUE+"Bienvenido, espero sea se tu agrado mi primer script. :D\n")
+    print(Fore.YELLOW+"-----------------------------------------------------------------\n")
+    print(Fore.BLUE+"Asegurate de estar en la carpeta donde vas a crear la carpeta y los archivos")
+    print(Fore.BLUE+"Ya sea el low level, el higher level o en la carpeta que gustes.")
+    print(Fore.YELLOW+"\n-----------------------------------------------------------------\n")
+    print(Fore.RED+"Ingrese a continuacion el nombre de la carpeta donde van todos los archivos Ejem: (0x15-python_hola)")
+    path = input(Fore.GREEN+"\n~ ").lower().strip("/")
     if exists(path) == False:
         subprocess.run(["mkdir", path])
     else:
-        print('Este directorio ya existe, quieres que se cree uno vacio? (Si o No)')
-        res = input("~ ")
+        print(Fore.RED+'\nEste directorio ya existe, quieres que se cree uno vacio? (Si o No)\n')
+        res = input(Fore.GREEN+"~ ")
         res.lower()
         if res == 'si':
             subprocess.run(["rm", "-rf", path])
             subprocess.run(["mkdir", path])
         elif (res == 'no'):
-            print("Vale, trabajaremos con el que ya existe")
-    print(path)
-    print("-----------------------------------------------------------------\n")
-    print("Ingrese el nombre de los archivos (por cada nombre presiona un enter, si ya terminaste, da enter)\n")
-    print("Para eliminar el ultimo archivo ingresado, escriba 'del' ")
-    print("Si pusiste un archivo repedito, no te preocupes que se elimina solito :3")
-    print("\nsin el README, ese te lo hago yo :D\n")
+            print(Fore.BLUE+"\nVale, trabajaremos con el que ya existe")
+    print(Fore.YELLOW+"-----------------------------------------------------------------\n")
+    print(Fore.BLUE+"Ingrese el nombre de los archivos (por cada nombre presiona un enter, si ya terminaste, da enter)\n")
+    print(Fore.MAGENTA+"Para eliminar el ultimo archivo ingresado, escriba 'del' ")
+    print(Fore.MAGENTA+"Si pusiste un archivo repetido, no te preocupes que se elimina solito :3")
+    print(Fore.RED+"\n -- El README te lo hago yo :D -- \n")
     pathv2 = path + "/"
     readme = pathv2 + 'README.md'
     while True:
-        archivos = input("~ ")
+        archivos = input(Fore.GREEN+"~ ")
         archivos.lower()
         if (len(archivos) > 0 and archivos != 'del'):
             todosArchivos.append(archivos.replace(" ", "_"))
@@ -47,9 +45,10 @@ try:
         else:
             index = todosArchivos[-1]
             todosArchivos.remove(index)
-            print(f"El siguiente archivo ha sido eliminado {index}")
-    comentario = input(
-        "Quieres que tus archivos tengan el PATH? (#!/usr/bin/node): ")
+            print(Fore.MAGENTA+f"\nEl siguiente archivo ha sido eliminado {index}")
+    print(Fore.YELLOW+"-----------------------------------------------------------------\n")
+    print(Fore.MAGENTA+f"Quieres que tus archivos tengan el PATH? Ejem: (#!/usr/bin/node)")
+    comentario = input(Fore.GREEN+"\n~ ")
     for i in range(len(todosArchivos)):
         if len(comentario) > 0:
             with open(f'{pathv2}{todosArchivos[i]}', 'w') as f:
@@ -63,8 +62,8 @@ try:
     f.write(path)
     f.close()
     os.system('clear')
-    print("Graicas por usar mi Script :D\n")
-    print("Todo se ha creado correctamente")
+    print(Fore.BLUE+"Graicas por usar mi Script :D\n")
+    print(Fore.BLUE+"Todo se ha creado correctamente")
 except KeyboardInterrupt:
     os.system("clear")
-    print("Se cerro el script")
+    print(Fore.BLUE+"Se cerro el script")
